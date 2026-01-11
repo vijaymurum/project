05a9d8e77718a12a8a029763a5de85a2726f7bda
vijaymurum <vjaykumar.066@gmail.com>
2026-01-11 22:48:59 +0530

    add tester2

--- src/test/java/com/test/ClassA.java
+++ src/test/java/com/test/ClassA.java
@@ -1,23 +1,39 @@
 package com.test;
 
 public class ClassA {
-	
+
 	public static void mobile() {
-System.out.println("Mobile");
+		System.out.println("Mobile");
 	}
+
 	public static void laptop() {
-System.out.println("Laptop");
+		System.out.println("Laptop");
 	}
+
 	public static void ipad() {
-System.out.println("ipad");
+		System.out.println("ipad");
 	}
-public static void main(String[] args) {
-	mobile();
-	laptop();
-	ipad();
-	
-}
-	
-	
-	
+
+	public static void tester2X() {
+		System.out.println("1");
+	}
+
+	public static void tester2Y() {
+		System.out.println("2");
+	}
+
+	public static void tester2Z() {
+		System.out.println("3");
+	}
+
+	public static void main(String[] args) {
+		mobile();
+		laptop();
+		ipad();
+		tester2X();
+		tester2Y();
+		tester2Z();
+
+	}
+
 }
