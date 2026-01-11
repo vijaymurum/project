1256098c007ccd51161e1698d0e9096f5804522b
vijaymurum <vjaykumar.066@gmail.com>
2026-01-11 21:54:02 +0530

    new

---
+++ src/test/java/com/test/ClassA.java
@@ -0,0 +1,23 @@
+package com.test;
+
+public class ClassA {
+	
+	public static void mobile() {
+System.out.println("Mobile");
+	}
+	public static void laptop() {
+System.out.println("Laptop");
+	}
+	public static void ipad() {
+System.out.println("ipad");
+	}
+public static void main(String[] args) {
+	mobile();
+	laptop();
+	ipad();
+	
+}
+	
+	
+	
+}
